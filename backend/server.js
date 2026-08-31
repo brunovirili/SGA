a05043602011f7f6a5a6419eca9482a906f76e40
virili <virili.bruno456@gmail.com>
2026-08-31 16:07:45 -0300

    Uso de POST y PUT

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const app = express()
+app.use(express.json())
 
 const alumnos = [
     {
@@ -29,6 +30,20 @@ app.get("/alumnos/:id", (req, res) => {
     res.json(alumno)
 })
 
+app.post("/alumnos", (req, res) =>{
+    const nuevoAlumno = req.body
+    alumnos.push(nuevoAlumno)
+    res.json({mensaje: "Alumno registrado correctamente."})
+})
+
+app.put("/alumnos/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const alumno = alumnos.find(alumno => alumno.id === id)
+    alumno.id = req.body.id
+    alumno.nombre = req.body.nombre
+    alumno.carrera = req.body.carrera
+    res.json({mensaje: "Alumno actualizado correctamente."})
+})
 app.listen(3000, () => {
     console.log("Servidor funcionando en http://localhost:3000")
 })
