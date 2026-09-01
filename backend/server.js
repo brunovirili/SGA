7e6c33d9304a2ebc6e9d57091aa863a15eabc89b
virili <virili.bruno456@gmail.com>
2026-09-01 15:35:57 -0300

    Clase 15 - DELETE, Middleware, router, controllers y data

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express")
 const app = express()
 app.use(express.json())
+const alumnosRoutes = require("./routes/alumnos.routes")
+app.use("/alumnos", alumnosRoutes)
 
-const alumnos = [
+let alumnos = [
     {
         id: 1,
         nombre: "Ana",
@@ -20,30 +22,17 @@ const alumnos = [
     }
 ]
 
-app.get("/alumnos", (req, res) => {
-    res.json(alumnos)
+// Creo Middleware
+
+app.use((req, res, next) => {
+    console.log(req.method)
+    console.log(req.url)
+    next()
 })
 
-app.get("/alumnos/:id", (req, res) => {
-    const id = Number(req.params.id)
-    const alumno = alumnos.find(a => a.id === id)
-    res.json(alumno)
-})
 
-app.post("/alumnos", (req, res) =>{
-    const nuevoAlumno = req.body
-    alumnos.push(nuevoAlumno)
-    res.json({mensaje: "Alumno registrado correctamente."})
-})
 
-app.put("/alumnos/:id", (req, res) => {
-    const id = Number(req.params.id)
-    const alumno = alumnos.find(alumno => alumno.id === id)
-    alumno.id = req.body.id
-    alumno.nombre = req.body.nombre
-    alumno.carrera = req.body.carrera
-    res.json({mensaje: "Alumno actualizado correctamente."})
-})
+
 app.listen(3000, () => {
     console.log("Servidor funcionando en http://localhost:3000")
 })
